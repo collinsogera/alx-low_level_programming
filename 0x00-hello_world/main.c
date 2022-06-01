182c9db3c865b39560fb1ca69e625ce1c461a83f
Collins Ogera <ogera.collins@gmail.com>
2022-06-01 13:23:23 -0700

    Task 0

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -4,9 +4,11 @@
 * main - Starting point of the program
 *
 * void parameter requires no arguement
+* printf displays standard output
 * Return: Always 0 signifies success
 */
 int main(void)
 {
+	printf("My name is Awesome!");
 	return (0);
 }
