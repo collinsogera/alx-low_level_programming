209c8290322dbadd9b2e0a31e390016cb42f9225
Collins Ogera <ogera.collins@gmail.com>
2022-06-01 13:11:48 -0700

    Run a C file though preprocessor and save

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Starting point of the program
+*
+* void parameter requires no arguement
+* Return: Always 0 signifies success
+*/
+int main(void)
+{
+	return (0);
+}
