9dc346da47af1557a899c6410ec88174e198128e
Collins Ogera <ogera.collins@gmail.com>
2022-06-04 05:35:52 -0700

    Program to determine if last digit is greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last = n % 10;
+	int last;
+
+	last = n % 10;
 
 	if (n > 5)
 		printf("Last digit of %d is %d and  is greater than 5 \n", n, last);
