d3ea777db2ad87e57d8e88cedb46ec3626373373
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 10:04:35 -0700

    Using a function to write to screen

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,16 @@
 * Return: 1 if success
 */
 int main(void)
-{	
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+{
+	char *put = "_putchar";
+
+	while (*put)
+	{
+		_putchar(*put);
+		put++;
+	}
 	_putchar('\n');
 
 	return (0);
 }
+
