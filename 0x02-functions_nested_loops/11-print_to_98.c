68be42d26c72089f0543067c058ccbe8ada9e907
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 14:03:00 -0700

    Print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_to_98 - Function to print natural numbers 
+*@n: First parameter
+*
+*/ 
+
+void print_to_98(int n)
+{
+	while(n <= 98)
+	{
+		_putchar(n);
+		n++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int _abs(int);
 int print_last_digit(int l);
 
 int add(int a, int b);
+void print_to_98(int n);
 
 #endif
