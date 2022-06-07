810c53edaf733f52361e59b01824549fa404be3f
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 14:28:21 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*_abs - Function to obtain absolute value
+*@a: First arguement
+* 
+* Return: 1 if successful
+*/
+
+int _abs(int a)
+{
+	return (a * ((a > 0) - (a < 0)));
+}
