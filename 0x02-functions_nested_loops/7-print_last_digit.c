d47724f417f5e472a867b87754a88a832b19b550
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 13:13:38 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_last_digit - Function to print last digit
+*
+*Return: ret is the remainder which should be the last digit
+*
+*/
+int print_last_digit(int)
+{
+	int a;
+
+	int ret;
+
+	ret = a % 10;
+
+	return ret;
+}
