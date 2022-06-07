afb6004d5f82e90d2ecba1b7f72ddbdff3ca3a5f
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 10:21:17 -0700

    Alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet - Function to print lowercase alphabets
+*
+*Return: Has no return value
+*
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+		_putchar(c);
+	_putchar('\n');
+
+}
+
