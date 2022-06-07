4925b996490c5821eb897d2869d5b5234aa86f1a
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 03:13:59 -0700

    Display output using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+*main - Starting point of program
+*
+*Description: Program to print _putchar
+*Return: 1 success -1 error
+*/
+int main(void)
+{
+	int ret = _putchar('_');
+	int p = _putchar('p');
+	int u = _putchar('u');
+	int t = _putchar('t');
+	int c = _putchar('c');
+	int h = _putchar('h');
+	int a = _putchar('a');
+	int r = _putchar('r');
+	int n = _putchar('\n');
+
+	return (0);
+}
