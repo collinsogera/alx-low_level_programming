8572f82dbd37408d97546137d7f68bc93523f548
Collins Ogera <ogera.collins@gmail.com>
2022-06-07 12:42:20 -0700

    Check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_sign - Function to determine sign of a number
+*@c: First parameter
+*
+* Return: 0 if positive, 0 if 0 else negative
+*/
+int print_sign(int c)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
