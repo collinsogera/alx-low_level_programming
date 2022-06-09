7dc073d49c968e0f1c9c1d1221842c35085af59e
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 13:32:13 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,27 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-*fizz_buzz - Function to print 1 - 100 while
+*main - Function to print 1 - 100 while
 *considering multiples of 3 and 5
 */
-void fizz_buzz(void)
+void main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i == 100)
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf(i);
+			printf("%d", i);
 		printf(" ");
 	}
+	printf("\n");
+	return (0);
 }
