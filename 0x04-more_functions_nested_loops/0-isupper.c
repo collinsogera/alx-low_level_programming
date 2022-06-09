91447574aa78a3917a548dc9fa4d0a81a6b1b456
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 09:43:30 -0700

    Confirm upper or lowercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isupper - Checks for uppercase characters
+*@c: Character passed in
+*
+* Return: 0 if uppercase, else 1
+*/
+int _isupper(int c)
+{
+	c = 'A';
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Directory containing more function programs and nested loops.
