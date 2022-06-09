fad3ab657484bef6aaa14603f08d906d9d6974c6
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 11:15:07 -0700

    Multiplication program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*mul - Program to multiply two integers
+*@a: First operand
+*@b: second operand
+*
+* Return: Product of the two values
+*/
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
+
