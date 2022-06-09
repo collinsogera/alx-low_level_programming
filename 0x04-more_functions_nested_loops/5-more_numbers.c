2a8f62388501a5c744eb4d026ca6cfa8d159d191
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 14:14:40 -0700

    Print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ void more_numbers(void)
 		for (c = 0; c <= 14; c++)
 		{
 			if (c < 10)
-				_putchar((c % 10) + '0');
+				_putchar((c / 10) + '0');
 			else
 			{
 				_putchar((c / 10) + '0');
