202e49280fa2c4d0a7a30d7eb802a9c968762e1a
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 14:30:30 -0700

    Printa triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,19 +7,19 @@
 */
 void print_triangle(int size)
 {
-	int a, b, c;
+	int a, b;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (a = 0; a <= (size - 1); a++)
+		for (a = 1; a <= size; a++)
 		{
-			for (b = 0; b <= (size - 1) - a; b++)
+			for (b = size - a; b > 0; b--)
 			{
 				_putchar(' ');
 			}
-			for (c = 0; c < a; c++)
+			for (b = 0; b < a; b++)
 				_putchar('#');
 			_putchar('\n');
 		}
