c7324cab14d19b2113949b9ffba1cd4d433daba9
Collins Ogera <ogera.collins@gmail.com>
2022-06-09 10:59:50 -0700

    Check for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+*_isdigit - Program to check for a digit
+*@c: digit parameter
+*
+* Return: 1 if digit, else 0
+*/
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
