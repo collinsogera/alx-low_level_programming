97c86e5d2451bd82cf16f99649f28b31f5e4c682
Collins Ogera <ogera.collins@gmail.com>
2022-06-17 06:02:39 -0700

    Encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,19 @@
 */
 char *leet(char  *n)
 {
-	int i, j;
-
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i] == s1[j])
+		for (x = 0; x <= 9; x++)
 		{
-			n[i] = s2[j];
+			if (n[i] == find[x])
+			{
+				n[i] = replacer[x / 2];
+				x = 9;
+			}
 		}
 	}
 
