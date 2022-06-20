0f7c60f17e188324f1775235940b16f308c361c6
Collins Ogera <ogera.collins@gmail.com>
2022-06-20 05:52:36 -0700

    Serch a string for set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strpbrk - Search a string for any set of bytes
+*@s: Pointer to byte
+*@accept: Pointer to bytes in second string
+*
+*Return: Pointer to byte s matching bytes in accept
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = o; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+				return (s + a);
+		}
+	}
+
+	return (NULL);
+}
+
