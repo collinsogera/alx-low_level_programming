cebaacd55db40eddd29375a8be5fa862dc11af60
Collins Ogera <ogera.collins@gmail.com>
2022-06-20 06:01:28 -0700

    Print Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_chessboard - Function to print a chessboard
+*@a: array
+*
+*/
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
+
