6b7e442c24fa5de4e7502ee7db3637e1993ad555
Collins Ogera <ogera.collins@gmail.com>
2022-06-21 09:36:37 -0700

    Print sum of diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,10 +9,16 @@ void print_diagsums(int *a, int size)
 {
 	int x, totala = 0, totalb = 0;
 
-	for (x = 0; x < size; x++)
+	int m = size * size;
+
+	for (x = 0; x < m; x += size + 1)
 	{
-		totala += a[(size + 1) * x];
-		totalb += a[((size - 1) * (x + 1)];
+		totala += a[x];
+	}
+
+	for (x = size - 1; x < m - 2; x += size - 1)
+	{
+		totalb += a[x];
 	}
 
 	printf("%d, %d\n", totala, totalb);
