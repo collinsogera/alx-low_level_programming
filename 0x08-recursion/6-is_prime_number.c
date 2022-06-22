5a4e47ffc348b451fec0b9191d14fc6f1de6e801
Collins Ogera <ogera.collins@gmail.com>
2022-06-22 11:40:44 -0700

    Check if number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,9 @@
  */
 int is_prime_number(int n)
 {
-	return (is_prime(n, 1));
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, n - 1));
 }
 
 /**
@@ -20,12 +22,9 @@ int is_prime_number(int n)
  */
 int is_prime(int n, int a)
 {
-	if (n <= 1)
-		return (0);
-	if (n % a == 0 && a > 1)
-		return (0);
-	if ((n / a) < 1)
+	if (a == 1)
 		return (1);
-
-	return (is_prime(n, a + 1));
+	if (n % a == 0 && a > 0)
+		return (0);
+	return (is_prime(n, a - 1));
 }
