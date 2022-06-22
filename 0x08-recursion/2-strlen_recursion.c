36f39e6b67afeb7ad662fc0e0a1b715fa7478580
Collins Ogera <ogera.collins@gmail.com>
2022-06-22 10:28:08 -0700

    Length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen_recursion - Returns length of a string
+*@s: Pointer to string
+*
+*Return: Length of string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	s++;
+	return (_srelen_recursion(s) + 1);
+}
+
