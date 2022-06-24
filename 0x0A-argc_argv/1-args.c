e9a94e56c395299bea17e04906758ac1378ed4fa
Collins Ogera <ogera.collins@gmail.com>
2022-06-24 10:11:38 -0700

    Print no. of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Print each given arguement
+ *@argc: counter
+ *@argc: String array
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+		printf("%s\n", argv[a]);
+
+	return (0);
+}
+
