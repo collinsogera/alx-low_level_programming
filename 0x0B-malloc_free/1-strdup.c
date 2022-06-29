bdc322299fa4f6767bc12b006e4b68ef32ac28b2
Collins Ogera <ogera.collins@gmail.com>
2022-06-29 09:47:10 -0700

    Pointer to new space allocated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - Return pointer to newly allocated space
+ *@str: Array
+ *
+ *Return: Pointer
+ */
+char *_strdup(char *str)
+{
+	int a, b;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	a = 0;
+
+	while (*(str + a) != '\0')
+		a++;
+
+	s = malloc(a + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (b = 0l b < a; b++)
+		*(s + b) = *(str + b);
+
+	return (s);
+}
