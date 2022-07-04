bfd1382073876d24d65b28525e4869f9765488a9
Collins Ogera <ogera.collins@gmail.com>
2022-07-04 12:15:59 -0700

    Dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-int _putchar(char c);
+
 /**
  *struct dog - One of the many pets
  *@name: Dog's name
  *@age: Its age
  *@owner: Who owns it
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
@@ -16,7 +16,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /*  DOG_H */
