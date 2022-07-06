180b515a3281ac658a2f1a24a6fb373c8e00ae2d
Collins Ogera <ogera.collins@gmail.com>
2022-07-05 22:41:07 -0700

    Performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - Check program
+ *@argc: Arguements cant
+ *@argv: Vector arguements
+ *Return: 1 if success
+ */
+int main(int argc, char *argv[])
+{
+	int (*get)(int, int);
+	int a, b, operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	operator = argv[2][0];
+	switch (operator)
+	{
+	case '+':
+		break;
+	case '-':
+		break;
+	case '*':
+		break;
+	case '/':
+		break;
+	case '%':
+		break;
+	default:
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	get = get_op_func(argv[2]);
+	printf("%d\n", get(a, b));
+	return (0);
+}
