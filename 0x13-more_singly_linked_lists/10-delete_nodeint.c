057e8b297ef4601d7f83b3cefcda018fa74ed445
Collins Ogera <ogera.collins@gmail.com>
2022-07-19 13:28:19 -0700

    Delete node at index of list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		tmp_node = tmp_node->next;
 		++i;
-		if (!tmo_node)
+		if (!tmp_node)
 			return (-1);
 	}
 	after_node = tmp_node->next->next;
