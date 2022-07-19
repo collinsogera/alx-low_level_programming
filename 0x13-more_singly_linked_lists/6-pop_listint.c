89ccc940d10960d45bc127dfbe38c297e3f31667
Collins Ogera <ogera.collins@gmail.com>
2022-07-19 12:51:22 -0700

    Deletes head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *pop_listint - Remove first item of a list
+ *@head: Pointer to first node
+ *Return: Value of node deleted
+ */
+int pop_listint(listint_t **head)
+{
+	int value = 0;
+	listint_t *next_node = NULL;
+
+	if (*head == NULL)
+		return (0);
+
+	next_node = (*head)->nextr;
+	value = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+	return (value);
+}
+
