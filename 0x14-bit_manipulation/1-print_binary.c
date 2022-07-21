13c9d69e2c8eba9d1231fc6b5cbd9efd7dedbd03
Collins Ogera <ogera.collins@gmail.com>
2022-07-21 11:21:05 -0700

    Print binary rep of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_binary - Function that prints binary rep of a number
+ *@n: Number to be converted
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
+}
+
